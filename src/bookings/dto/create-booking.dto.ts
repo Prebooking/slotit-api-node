2d75993b9346b89041c0aef1b738b563179eea24
amal <amalraju9598@gmail.com>
2025-02-04 11:31:40 +0530

    dto update

--- src/bookings/dto/create-booking.dto.ts
+++ src/bookings/dto/create-booking.dto.ts
@@ -1,10 +1,27 @@
+import { Type } from 'class-transformer';
 import {
   IsString,
   IsOptional,
   IsDateString,
   IsNotEmpty,
+  IsArray,
+  IsObject,
+  IsPhoneNumber,
+  ValidateNested,
+  IsUUID,
+  IsDate,
+  ArrayMinSize,
 } from 'class-validator';
 
+class UserDetails {
+  @IsNotEmpty()
+  @IsString()
+  first_name: string;
+
+  @IsNotEmpty()
+  @IsPhoneNumber(null)
+  mobile: string;
+}
 export class CreateBookingDto {
   @IsOptional()
   @IsString()
@@ -30,4 +47,41 @@ export class CreateBookingDto {
 
   @IsNotEmpty()
   date?: string;
+
+  
 }
+
+export class CreateBookingAdminDto {
+  @IsOptional()
+  @IsString()
+  user_id?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  shop_room_id?: string;
+
+  @IsNotEmpty()
+  @IsArray()
+  shop_service_ids?: any[];
+
+  @IsOptional()
+  @IsString()
+  note?: string;
+
+  @IsNotEmpty()
+  time_from?: string;
+
+  @IsNotEmpty()
+  time_to?: string;
+
+  @IsNotEmpty()
+  date?: string;
+
+  @IsNotEmpty()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => UserDetails)
+  user: UserDetails;
+}
+
+
